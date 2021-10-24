876d6a0c125655838ebbbd05a5e9501c6aa3d2ad
akiko.kano <47952174+ichirin-planet@users.noreply.github.com>
2021-10-24 18:12:12 +0900

    finished

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SarchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-// import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  image: "https://source.unsplash.com/2l0CWTpcChI",
-  name: "亜紀子",
-  email: "1234.ex.com",
-  phone: "090-4444-3333",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,11 +1,44 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    image: "https://source.unsplash.com/2l0CWTpcChI",
+    name: `亜紀子${val}`,
+    email: "1234.ex.com",
+    phone: "090-4444-3333",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "google.com"
+  };
+});
+
 export const Users = () => {
   return (
     <SContainer>
-      <h2>ユーザー一覧</h2>
+      <SUserSrea>
+        <h2>ユーザー一覧</h2>
+        <SearchInput />
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserSrea>
     </SContainer>
   );
 };
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserSrea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
